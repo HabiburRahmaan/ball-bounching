3c75bddc567d11d463731d4374a942256f58b93f
Habibur Rahman <rahmanhabibur.cse@gmail.com>
2020-01-15 22:48:17 +0600

    first commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("hello ball");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnMouseDown()
+    {
+        GameObject.Find("GameManager").GetComponent<GameManager>().Scoreup();
+        Destroy(gameObject);   
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject WinText;
+    public GameObject restartBTN;
+    int Score = 0;
+   
+
+    
+    void Update()
+    {
+        
+    }
+    public void Scoreup()
+    {
+        Score++;
+        if (Score>=8)
+        {
+
+            win();
+        }
+    }
+
+    void win()
+    {
+        WinText.SetActive(true);
+        restartBTN.SetActive(true);
+    }
+
+public void Restart()
+    {
+        SceneManager.LoadScene("Game");
+
+    }
+
+    
+}
